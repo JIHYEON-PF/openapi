1e4757f328002f6b531a566f3fa211774e6d07f3
JIHYEON <jhkim@aptner.com>
2023-11-30 23:18:45 +0900

    feature:#4 - 랜덤단어 복수 조회 방식으로 변경
    - 랜덤 단어를 복수개 조회할 수 있도록 기능 수정
    - 파라미터의 Default는 0

---
+++ src/main/java/com/jh/openapi/randomword/domain/response/WordResponsesDto.java
@@ -0,0 +1,27 @@
+package com.jh.openapi.randomword.domain.response;
+
+import lombok.Getter;
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class WordResponsesDto {
+    private final List<WordResponseDto> words = new ArrayList<>();
+
+    public static WordResponsesDto from(List<WordResponseDto> words) {
+        return new WordResponsesDto(words);
+    }
+
+    public static WordResponsesDto empty() {
+        return new WordResponsesDto(List.of(WordResponseDto.emptyWord()));
+    }
+
+    private WordResponsesDto(List<WordResponseDto> words) {
+        if (BooleanUtils.isFalse(CollectionUtils.isEmpty(words))) {
+            this.words.addAll(words);
+        }
+    }
+}
--- src/main/java/com/jh/openapi/randomword/repository/WordRepository.java
+++ src/main/java/com/jh/openapi/randomword/repository/WordRepository.java
@@ -1,16 +1,30 @@
 package com.jh.openapi.randomword.repository;
 
 import com.jh.openapi.randomword.domain.entity.EnglishWord;
-import com.jh.openapi.randomword.domain.type.WordLevelType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface WordRepository extends JpaRepository<EnglishWord, Long> {
-    List<EnglishWord> findEnglishWordByLevel(WordLevelType level);
+    @Query(value = "SELECT WORD.idx, " +
+                   "       WORD.vocabulary, " +
+                   "       WORD.meaning, " +
+                   "       WORD.level," +
+                   "       WORD.reg_user_nickname, " +
+                   "       WORD.reg_datetime, " +
+                   "       WORD.mod_user_nickname, " +
+                   "       WORD.mod_datetime " +
+                   "FROM   openapi_db.english_word AS WORD " +
+                   "WHERE  WORD.level IN :levelTypes " +
+                   "ORDER BY RAND() " +
+                   "LIMIT  :count", nativeQuery = true)
+    List<EnglishWord> findAllOrderByRandomLimitBy(@Param("levelTypes") Set<String> levelTypes, @Param("count") Long count);
 
     Optional<EnglishWord> findEnglishWordByVocabulary(String vocabulary);
 }
